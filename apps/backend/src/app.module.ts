a024ec52b70e2f76d7e47daeb7078a8b6fd186c8
Boamah-Powers <kb4242@nyu.edu>
2025-03-25 22:56:57 +0400

    removed backend to start dev

--- apps/backend/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from '@app/modules/auth/auth.module';
-import { UserModule } from '@app/modules/user/user.module';
-import { PrismaModule } from '@app/prisma/prisma.module';
-
-@Module({
-  imports: [PrismaModule, AuthModule, UserModule],
-})
-export class AppModule {}
