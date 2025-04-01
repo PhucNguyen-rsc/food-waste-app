71c7fe90a77ccac240665d0630f1edf22373c76c
Boamah-Powers <kb4242@nyu.edu>
2025-04-02 01:28:24 +0400

    security fix

--- apps/mobile/src/config/firebaseConfig.ts
+++ apps/mobile/src/config/firebaseConfig.ts
@@ -4,13 +4,13 @@ import auth from '@react-native-firebase/auth';
 import firebase from '@react-native-firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBBBNbediV1T_poE7gigOPtLsHo88rqU7U',
-  authDomain: 'food-waste-app-ec4ce.firebaseapp.com',
-  projectId: 'food-waste-app-ec4ce',
-  storageBucket: 'food-waste-app-ec4ce.appspot.com', // typically ends with "appspot.com"
-  messagingSenderId: '942470974605',
-  appId: '1:942470974605:web:68aa85c76be1f248bbe6c4',
-  measurementId: 'G-XN8SSN28YZ',
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase if it hasn't been initialized yet
